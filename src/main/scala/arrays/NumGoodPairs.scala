8924e878cc4997cf519b3bc9a5b6568e6ff7e570
kunaltiwary <kunaltiwary@gmail.com>
2023-08-14 08:40:26 +1000

    Updated with comments

--- src/main/scala/arrays/NumGoodPairs.scala
+++ src/main/scala/arrays/NumGoodPairs.scala
@@ -7,8 +7,15 @@ object NumGoodPairs extends App {
     import scala.collection.mutable
     val seen = new mutable.HashMap[Int, Int]()
     var result = 0
+    //Result: 0 Num: 1 Count: 0
+    //Result: 0 Num: 2 Count: 0
+    //Result: 0 Num: 3 Count: 0
+    //Result: 1 Num: 1 Count: 1
+    //Result: 3 Num: 1 Count: 2
+    //Result: 4 Num: 3 Count: 1
     for (n <- nums) {
       result += seen.getOrElse(n, 0)
+      println(s"Result: $result Num: $n Count: ${seen.getOrElse(n, 0)}")
       seen.updateWith(n) {
         case Some(value) => Some(value + 1)
         case None => Some(1)
